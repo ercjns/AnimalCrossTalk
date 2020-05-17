9ae49c3978246e1b65d20f3ebf083312d6167a8f
ercjns <ercjns@users.noreply.github.com>
2020-05-17 03:45:21 +0000

    Github Actions - Sun May 17 03:45:21 UTC 2020

---
+++ assets/js/search-data.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "s1e1: Naomi Kyle",
+        "excerpt":"","categories": ["season-1"],
+        "tags": [],
+        "url": "https://ercjns.com/AnimalCrossTalk/season-1/s1e1/"
+      },{
+        "title": "s1e2: Leah Whitta, Tanya",
+        "excerpt":"Notable   This was the first time that Gary’s Twitch stream title used the show’s name.","categories": ["season-1"],
+        "tags": [],
+        "url": "https://ercjns.com/AnimalCrossTalk/season-1/s1e2/"
+      }]
